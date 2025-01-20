3aaffb570dbd9eebc588042992e5060b7e7e5562
eidelsafira <127928028+DwiyahantaEidelS@users.noreply.github.com>
2025-01-20 20:17:46 +0700

    Update menteri.php

--- menteri.php
+++ menteri.php
@@ -16,13 +16,3 @@ class menteri extends pengurusBEM {
      echo "$this->jabatan";
   }
 }
-
-// $pengurus1 = new menteri('Eidel Safira');
-// $pengurus1->setNIM('225150707111234');
-// $pengurus1->setJabatan('Menteri Komunikasi dan Informasi');
-// echo "<br><br>";
-// $pengurus1->getNama();
-// echo "<br>";
-// $pengurus1->getJabatan();
-// echo "<br>";
-// $pengurus1->getNIM();
